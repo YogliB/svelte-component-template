2f54b16771e831afde54bc68e5788bf67ad3e233
YogliB <yogiboaron@gmail.com>
2019-06-01 00:04:59 +0300

    First REAL commit

---
+++ src/index.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<h1>Hellow World!</h1>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import index from './index.svelte';
+
+const oomponent = new index({
+  target: document.body,
+  props: {},
+});
+
+export default app;
