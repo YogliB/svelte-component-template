2687f63ad15db2fe66ca364302c89abcf5931e3f
YogliB <yogiboaron@gmail.com>
2019-06-22 14:08:46 +0300

    Prevent from CSS from being omited

--- rollup.config.js
+++ rollup.config.js
@@ -33,11 +33,6 @@ export default {
     svelte({
       // enable run-time checks when not in production
       dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: (css) => {
-        css.write('public/bundle.css');
-      },
       /**
        * Auto preprocess supported languages with
        * '<template>'/'external src files' support
