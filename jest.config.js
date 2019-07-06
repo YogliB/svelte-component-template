145ccdb596caf0993e9a27c5ea4f14f08b578ddd
YogliB <yogiboaron@gmail.com>
2019-07-06 22:27:40 +0300

    Add `jest` config file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.svelte$': 'jest-transform-svelte',
+  },
+  moduleFileExtensions: ['js', 'svelte'],
+  bail: false,
+  verbose: false,
+};
