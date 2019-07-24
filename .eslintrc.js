c49c49d6002fc9084a1c3c3e41ff5204f51bcad2
YogliB <yogiboaron@gmail.com>
2019-07-24 22:38:54 +0300

    Prettify files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    'jest/globals': true,
-  },
-  extends: ['eslint:recommended', 'plugin:jest/recommended', 'prettier'],
-  overrides: [
-    {
-      files: '*.svelte',
-      processor: 'svelte3/svelte3',
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: 'module',
-  },
-  plugins: ['svelte3', 'jest'],
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+		'jest/globals': true,
+	},
+	extends: ['eslint:recommended', 'plugin:jest/recommended', 'prettier'],
+	overrides: [
+		{
+			files: '*.svelte',
+			processor: 'svelte3/svelte3',
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 2019,
+		sourceType: 'module',
+	},
+	plugins: ['svelte3', 'jest'],
 };
