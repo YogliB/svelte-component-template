3daed717029a4ef8196efe0397b6a041b86c5702
YogliB <yogiboaron@gmail.com>
2019-08-07 22:51:28 +0300

    Move around files to `packages/**` path

---
+++ packages/my-component/rollup.config.js
@@ -0,0 +1,78 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "./node_modules/rollup-plugin-node-resolve";
+import commonjs from "./node_modules/rollup-plugin-commonjs";
+import livereload from "./node_modules/rollup-plugin-livereload";
+import { terser } from "./node_modules/rollup-plugin-terser";
+import pkg from "./package.json.js";
+import autoPreprocess from "./node_modules/svelte-preprocess";
+import babel from "./node_modules/rollup-plugin-babel";
+
+const production = !process.env.ROLLUP_WATCH;
+const name = pkg.name
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
+  .replace(/^\w/, m => m.toUpperCase())
+  .replace(/-\w/g, m => m[1].toUpperCase());
+
+export default {
+  input: !production ? "src/main.js" : "src/components/components.module.js",
+  output: !production
+    ? {
+        sourcemap: true,
+        format: "iife",
+        name: "app",
+        file: "public/bundle.js"
+      }
+    : [
+        {
+          file: pkg.module,
+          format: "es",
+          sourcemap: true,
+          name
+        },
+        {
+          file: pkg.main,
+          format: "umd",
+          sourcemap: true,
+          name
+        }
+      ],
+  plugins: [
+    babel({
+      runtimeHelpers: true
+    }),
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+
+      /**
+       * Auto preprocess supported languages with
+       * '<template>'/'external src files' support
+       **/
+      preprocess: autoPreprocess({
+        postcss: true,
+        scss: { includePaths: ["src", "node_modules"] }
+      })
+    }),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    commonjs({
+      include: ["node_modules/**"]
+    }),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+};
