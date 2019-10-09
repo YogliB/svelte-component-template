e3118ca200eb26e63b1252c46167fa7aacf429f1
Yogev Boaron Ben-Har <yogev@oz-tms.com>
2019-10-09 20:18:10 +0300

    Update test configs based on testing-library docs

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.svelte$': 'jest-transform-svelte',
-  },
-  moduleFileExtensions: ['js', 'svelte'],
-  bail: false,
-  verbose: false,
-  setupFilesAfterEnv: [
-    '@testing-library/svelte/cleanup-after-each',
-    '@testing-library/jest-dom/extend-expect',
-  ],
+	transform: {
+		'^.+\\.js$': 'babel-jest',
+		'^.+\\.svelte$': 'svelte-jest',
+	},
+	moduleFileExtensions: ['js', 'json', 'svelte'],
+	bail: false,
+	verbose: true,
+	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
