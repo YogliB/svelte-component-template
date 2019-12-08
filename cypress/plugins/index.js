de26159701e07f639584433e7611f567cc31f7ef
YogliB <yogiboaron@gmail.com>
2019-12-08 23:50:01 +0200

    Add missing pkg and fix Cypress issue

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,8 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// module.exports = (on, config) => {
-// `on` is used to hook into various events Cypress emits
-// `config` is the resolved Cypress config
-// }
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+	// `on` is used to hook into various events Cypress emits
+	// `config` is the resolved Cypress config
+};
