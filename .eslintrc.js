32f795150776080d8bc70ff67ba0378382fa73de
Yogev Boaron Ben-Har <yogiboaron@gmail.com>
2020-03-01 22:47:55 +0200

    ci: Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:cypress/recommended', 'prettier'],
 	overrides: [
 		{
-			files: '*.svelte',
+			files: ['**/*.svelte'],
 			processor: 'svelte3/svelte3',
 		},
 	],
