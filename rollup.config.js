105e8942785deccb5aeda0961026182b8f01b73f
Yogev Boaron Ben-Har <yogiboaron@gmail.com>
2020-03-01 22:38:28 +0200

    refactor: Simplify build config

--- rollup.config.js
+++ rollup.config.js
@@ -66,8 +66,7 @@ export default {
 		// https://github.com/rollup/rollup-plugin-commonjs
 		resolve({
 			browser: true,
-			dedupe: (importee) =>
-				importee === 'svelte' || importee.startsWith('svelte/'),
+			dedupe: ['svelte'],
 		}),
 		commonjs({
 			include: ['node_modules/**'],
