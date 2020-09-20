9d52aeeaab1c13a85d7c6cdd725e9c4e8f75d1b6
YogliB <yogiboaron@gmail.com>
2020-09-20 14:16:52 +0300

    simplify code & fix test

---
+++ src/components/MyComponent.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {string} */
+	export let name = 'World';
+</script>
+
+<style>
+	h1 {
+		margin: auto;
+		color: #ff3e00;
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 5rem;
+		font-weight: lighter;
+		text-align: center;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import { default as MyComponent } from './MyComponent.svelte';
+
+export { MyComponent };
