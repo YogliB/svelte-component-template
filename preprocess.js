a5443be92728f462cf9b5a918a21775c6cae4bbb
YogliB <yogiboaron@gmail.com>
2020-09-20 19:57:34 +0300

    move `browserslist` and babel config

--- preprocess.js
+++ preprocess.js
@@ -17,7 +17,6 @@ const main = () => {
 		}
 		// listing all files using forEach
 		files.forEach(async (file) => {
-			console.log('file', JSON.stringify(file));
 			// load file
 			const sourceFile = fs.readFileSync(file, 'utf-8');
 			const distFile = file.replace('/src/components/', '/dist/');
@@ -41,22 +40,7 @@ const parseSvelte = async (source, destination) => {
 		const item = await svelte.preprocess(
 			source,
 			autoprocessor({
-				babel: {
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								loose: true,
-								// No need for babel to resolve modules
-								modules: false,
-								targets: {
-									// ! Very important. Target es6+
-									esmodules: true,
-								},
-							},
-						],
-					],
-				},
+				babel: true,
 				postcss: {
 					plugins: [require('autoprefixer')],
 				},
