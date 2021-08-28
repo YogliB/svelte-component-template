8cb87112edd99ecbaf604aa0372e3a99b5e3b97c
YogliB <yogiboaron@gmail.com>
2021-08-28 12:28:15 +0300

    feat: add components and set exports

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+import { default as First } from './first/First.svelte';
+import { default as Second } from './second/Second.svelte';
+
+export { First, Second };
---
+++ src/lib/second/Secons.svelte
@@ -0,0 +1,16 @@
+<script lang="typescript">
+	export let name = 'World';
+	export let textColor = '#ff3e00';
+</script>
+
+<h1 style="color: {textColor}">Hello {name}!</h1>
+
+<style>
+	h1 {
+		margin: auto;
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 5rem;
+		font-weight: lighter;
+		text-align: center;
+	}
+</style>
