80ec66fc694a8b0191e743a1ff0f28ad702505f7
YogliB <yogiboaron@gmail.com>
2021-08-28 17:02:24 +0300

    docs: add the first story

---
+++ src/lib/first/First.stories.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import First from './First.svelte';
+</script>
+
+<Meta title="First" component={First} />
+
+<Story name="Primary">
+	<First name="Lucy" textColor="indianred" />
+</Story>
