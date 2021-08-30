1ceb3579fc4c23cd7cec3e807dad5e3477317939
YogliB <yogiboaron@gmail.com>
2021-08-30 11:05:01 +0300

    feat: add the First component to the app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+	import { First } from '$lib';
+</script>
+
+<First name="World" textColor="yellowgreen" />
