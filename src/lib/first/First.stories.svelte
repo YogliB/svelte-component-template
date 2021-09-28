0b5823d1ba5cf5b239b665ba8e90e2a8d672a64b
YogliB <me@yogev.dev>
2021-09-28 22:26:30 +0300

    refactor: remove jest, use cypress & add chromatic

---
+++ src/lib/first/First.stories.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import First from './First.svelte';
+</script>
+
+<!-- 👇 Creates specific parameters for the story -->
+<Meta
+	title="Components/First"
+	component={First}
+	argTypes={{
+		name: { control: 'text' },
+		textColor: { control: 'color' },
+	}}
+/>
+
+<Template let:args>
+	<First {...args} />
+</Template>
+
+<Story
+	name="Jane"
+	args={{
+		name: 'Jane',
+		textColor: 'lightblue',
+	}}
+/>
+
+<Story
+	name="John"
+	args={{
+		name: 'John',
+		textColor: 'indianred',
+	}}
+/>
+
+<Story
+	name="Jina"
+	args={{
+		name: 'Jina',
+		textColor: 'darkgray',
+	}}
+/>
